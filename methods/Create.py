38309b2f5a820d28aa3fb8288953af7b324514be
frankiebaffa <frankiebaffa@gmail.com>
2018-05-11 15:12:51 -0400

    isolate methods from Main.py

---
+++ methods/Create.py
@@ -0,0 +1,63 @@
+import os
+import xlwt
+import lib
+import classes
+import db
+from lib import settings
+from lib import FormatSheet
+from lib import PopulateSheet
+from db import Database
+from classes import CharacterClass
+
+def CreateNew():
+    bookName = 'test'
+    classlist = settings.classlist
+    racelist = settings.racelist
+
+    player = input('Who is playing the character?  ')
+    characterName = input('What is the name of your character?  ')
+
+    classcheck = False
+    while classcheck == False:
+        y = 0
+        for i in range(len(classlist)):
+            print('{} - {}'.format(y, classlist[i]))
+            y += 1
+        classaction = input('Pick a class for your character by number:  ')
+        try:
+            classaction = int(classaction)
+            characterClass = classlist[classaction]
+            classcheck = True
+            print('')
+        except:
+            print('Please pick a listed number.')
+            print('')
+
+    racecheck = False
+    while racecheck == False:
+        y = 0
+        for i in range(len(racelist)):
+            print('{} - {}'.format(y, racelist[i]))
+            y += 1
+        raceaction = input('Pick a race for your character by number:  ')
+        try:
+            raceaction = int(raceaction)
+            race = racelist[raceaction]
+            racecheck = True
+            print('')
+        except:
+            print('Please pick a listed number.')
+            print('')
+
+    character = CharacterClass.Character(characterName, characterClass, player,
+        race, classlist, racelist)
+
+    Database.dbAddNew(character)
+
+    FormatSheet.initFormat(settings.book, settings.sheet)
+
+    PopulateSheet.initSheet(settings.book, settings.sheet)
+
+    PopulateSheet.fillSheet(settings.book, settings.sheet)
+
+    settings.book.save('Excel/' + bookName + '.xls')
