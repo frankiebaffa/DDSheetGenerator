a2c8d11dbce84f403d2fbc3dcc53f606738bfb3c
frankiebaffa <frankiebaffa@gmail.com>
2018-05-16 13:44:44 -0400

    add global

--- lib/settings.py
+++ lib/settings.py
@@ -14,3 +14,16 @@ def init():
     global racelist
     racelist = ['Human', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Half-Orc',
         'Halfling']
+
+    global skillList
+
+    skillList = ['Appraise', 'Balance', 'Bluff', 'Climb', 'Concentrate', 'Craft1Type', 'Craft1',
+        'Craft2Type', 'Craft2', 'Craft3Type', 'Craft3', 'DecipherScript', 'Diplomacy', 'DisableDevice',
+        'Disguise', 'EscapeArtist', 'Forgery', 'GatherInfo', 'HandleAnimal', 'Heal', 'Hide',
+        'Intimidate', 'Jump', 'Knowledge1Type', 'Knowledge1', 'Knowledge2Type', 'Knowledge2',
+        'Knowledge3Type', 'Knowledge3', 'Knowledge4Type', 'Knowledge4', 'Knowledge5Type',
+        'Knowledge5', 'Listen', 'MoveSilently', 'OpenLock', 'Perform1Type', 'Perform1',
+        'Perform2Type', 'Perform2', 'Perform3Type', 'Perform3', 'Profession1Type', 'Profession1',
+        'Profession2Type', 'Profession2', 'Ride', 'Search', 'SenseMotive', 'SleightOfHand',
+        'Spellcraft', 'Spot', 'Survival', 'Swim', 'Tumble', 'UseMagicDevice', 'UseRope', 'Custom1type',
+        'Custom1', 'Custom2Type', 'Custom2']
